b94dd75c622845cd0b43ebad75901c52a2ffb840
kanjanaw <kanjanaw00@gmail.com>
2022-03-19 23:29:03 +0700

    fix style player block

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+const playerPieces = document.querySelectorAll(".player-pieces");
+playerPieces.forEach((playerPiece) => 
+  playerPiece.style.backgroundColor = "rgb(25, 212, 184)"
+);
+
 const bgPieces = document.querySelectorAll(".boardgame-pieces");
 bgPieces.forEach((bgPiece) => {
-    bgPiece.style.backgroundColor = 'rgb(220, 220, 220)'; // color must be in rgb(xxx, xxx, xxx) form only!
-    bgPiece.addEventListener("click", selectAndDeselect)
+  bgPiece.style.backgroundColor = "rgb(220, 220, 220)"; // color must be in rgb(xxx, xxx, xxx) form only!
+  bgPiece.addEventListener("click", selectAndDeselect);
 });
 
 function selectAndDeselect(event) {
-    // Piece in grey turn to player's color == player select this piece
-    if(event.currentTarget.style.backgroundColor == 'rgb(220, 220, 220)'){
-        this.style.backgroundColor = 'rgb(25, 212, 184)';
-        event.stopPropagation()
+  // Piece in grey turn to player's color == player select this piece
+  if (event.currentTarget.style.backgroundColor == "rgb(220, 220, 220)") {
+    this.style.backgroundColor = "rgb(25, 212, 184)";
+    event.stopPropagation();
 
     // Piece in player's color turn to grey == player deselect this piece
-    }else if(event.currentTarget.style.backgroundColor == 'rgb(25, 212, 184)'){
-        this.style.backgroundColor = 'rgb(220, 220, 220)';
-        event.stopPropagation()
-    }
+  } else if (event.currentTarget.style.backgroundColor == "rgb(25, 212, 184)") {
+    this.style.backgroundColor = "rgb(220, 220, 220)";
+    event.stopPropagation();
+  }
 }
