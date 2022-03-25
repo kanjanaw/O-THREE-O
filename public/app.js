36064cc8d0246be526e975a305d59d28a5f892f3
kanjanaw <kanjanaw00@gmail.com>
2022-03-25 19:40:35 +0700

    update boardgame

--- public/app.js
+++ public/app.js
@@ -9,17 +9,40 @@ bgPieces.forEach((bgPiece) => {
   bgPiece.addEventListener("click", selectAndDeselect);
 });
 
+var selectPieceSize;
+
 function selectAndDeselect(event) {
+    const pieceSelect = event.currentTarget.getAttribute('id');
+    selectPieceSize = pieceSelect[pieceSelect.length - 3];
+
   // Piece in grey turn to player's color == player select this piece
   if (event.currentTarget.style.backgroundColor == "rgb(220, 220, 220)") {
     this.style.backgroundColor = "rgb(25, 212, 184)";
+    selectPlayerPiece(selectPieceSize);
     event.stopPropagation();
 
     // Piece in player's color turn to grey == player deselect this piece
   } else if (event.currentTarget.style.backgroundColor == "rgb(25, 212, 184)") {
     this.style.backgroundColor = "rgb(220, 220, 220)";
+    selectPlayerPiece(selectPieceSize);
     event.stopPropagation();
   }
+}
+
+function selectPlayerPiece(size) {
+  const playerPieceSize = playerPieces.forEach((playerPiece) => {
+  const playerPieceID = playerPiece.getAttribute('id')
+  const playerPieceSize = playerPieceID[playerPieceID.length - 3]
+  const countPlayerPiece = 0;
+
+  if(playerPiece.style.backgroundColor == "rgb(25, 212, 184)" && size == playerPieceSize){
+    console.log(playerPieceSize);
+    playerPiece.style.backgroundColor = "rgb(220, 220, 220)";
+  } else if(playerPiece.style.backgroundColor == "rgb(220, 220, 220)" && size == playerPieceSize){
+    console.log(playerPieceSize);
+    playerPiece.style.backgroundColor = "rgb(25, 212, 184)";
+  }
+  })
 }
 
 // const logoutItems = document.querySelectorAll(".logged-out");
