761e469d3c1e2767862ba7828e6a4695a2ba6225
YANADASANG <neonhardpunch@gmail.com>
2022-05-06 13:44:23 +0700

    turn board can show cur player

--- public/game.js
+++ public/game.js
@@ -1,20 +1,30 @@
-
-
 function turnGame(snapshot) {
-   let curPlayer;
-    ref.child(roomCode).on("value", (snapshot) => {
-        snapshot.forEach((data) => {
-            curPlayer = snapshot.val().currentPlayer;
-        })
-        
-        
-        if(playerColor == curPlayer){
-            btnDoneClass.disabled = false;
-            playerNamesInGame[indexSetColor].style.opacity = 1;
-        }else{
-            btnDoneClass.disabled = true;
-            playerNamesInGame[indexSetColor].style.opacity = .3;
-        }
+  let curPlayer;
+  btnDoneClass.disabled = true;
+  playerNamesInGame.forEach(playerNamesInGame => {
+      playerNamesInGame.style.opacity = .3
+  });
+  ref.child(roomCode).on("value", (snapshot) => {
+    snapshot.forEach((data) => {
+      curPlayer = snapshot.val().currentPlayer;
     });
-    
+    if (playerColor == curPlayer) {
+      btnDoneClass.disabled = false;
+      playerNamesInGame[indexSetColor].style.opacity = 1;
+    } 
+    else{
+      if(curPlayer == "rgb(109, 187, 255)"){
+        playerNamesInGame[0].style.opacity = 1;
+      }
+      else if(curPlayer == "rgb(25, 212, 184)"){
+        playerNamesInGame[1].style.opacity = 1;
+    }
+    else if(curPlayer == "rgb(255, 222, 89)"){
+        playerNamesInGame[2].style.opacity = 1;
+    }
+    else if(curPlayer == "rgb(255, 109, 109)"){
+        playerNamesInGame[3].style.opacity = 1;
+    }  
+    }
+  });
 }
